731233834d0eefae54a4c5e5ee9f152add541a01
Jordan Powers <jordanpowers1227@gmail.com>
2018-05-29 19:48:43 -0700

    Sort order buttons

--- web/index.php
+++ web/index.php
@@ -15,39 +15,70 @@ require_once('functions.php');
         foreach($_POST["delfiles"] as $fileid) {
           $response = deleteFile($fileid);
           if(!(isset($response["ok"]) && $response["ok"])) {
-            die("Error deleting file: ".json_encode($response));
+            if($response["error"] == "file_deleted") {
+              echo("<p>File ".$fileid." already deleted</p>");
+            } else {
+              die("Error deleting file: ".json_encode($response));
+            }
           }
         }
       }
 
     }
+
+    if(isset($_GET["sort"])) {
+      $sort = htmlentities($_GET["sort"]);
+    } elseif($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["sort"])) {
+      $sort = $_POST["sort"];
+    } else {
+      $sort = "newest";
+    }
+    # See if the user specified a page, defaulting to page 1
+    if(isset($_GET["page"])) {
+      $page = $_GET["page"];
+    } elseif($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["page"])) {
+      $page = $_POST["page"];
+    }else {
+      $page = 1;
+    }
+
      ?>
     <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
       <table>
         <tr>
           <th>Name</th>
-          <th>Size</th>
-          <th>Date</th>
+          <th>
+            <?php
+            if($sort == "biggest") {
+              echo('<a href="'.htmlentities($_SERVER["PHP_SELF"]).'?page='.$page.'&sort=smallest">Size&#9660;</a>');
+            } elseif($sort == "smallest") {
+              echo('<a href="'.htmlentities($_SERVER["PHP_SELF"]).'?page='.$page.'&sort=biggest">Size&#9650;</a>');
+            } else {
+              echo('<a href="'.htmlentities($_SERVER["PHP_SELF"]).'?page='.$page.'&sort=biggest">Size</a>');
+            }
+            ?>
+          </th>
+          <th>
+            <?php
+            if($sort == "oldest") {
+              echo('<a href="'.htmlentities($_SERVER["PHP_SELF"]).'?page='.$page.'&sort=newest">Date&#9660;</a>');
+            } elseif($sort == "newest") {
+              echo('<a href="'.htmlentities($_SERVER["PHP_SELF"]).'?page='.$page.'&sort=oldest">Date&#9650;</a>');
+            } else {
+              echo('<a href="'.htmlentities($_SERVER["PHP_SELF"]).'?page='.$page.'&sort=oldest">Date</a>');
+            }
+            ?>
+          </th>
           <th>Delete?</th>
         </tr>
         <?php
         # Limit number of results per page. Might make this configurable by the user in the future
         $limit = 50;
 
-        # See if the user specified a page, defaulting to page 1
-        if(isset($_GET["page"])) {
-          $page = $_GET["page"];
-        } else {
-          $page = 1;
-        }
 
         $files = listAllFiles();
 
-        if(isset($_GET["sort"])) {
-          $sort = htmlentities($_GET["sort"]);
-        } else {
-          $sort = "newest";
-        }
+
         switch($sort) {
           case "newest":
             usort($files, "compare_newest");
@@ -84,6 +115,8 @@ require_once('functions.php');
          ?>
       </table>
       <input type="submit" value="Delete">
+      <input type="hidden" name="page" value="<?php echo(htmlentities($page)); ?>">
+      <input type="hidden" name="sort" value="<?php echo(htmlentities($sort)); ?>">
     </form>
     <?php
     if($page > 1) {
